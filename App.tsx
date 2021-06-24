a19882b24d31a79d6788a1569a0958fc6122bbaa
Dima <dmitry.balandin@cimpleo.com>
2021-06-24 14:40:36 +0600

    Elements added

--- App.tsx
+++ App.tsx
@@ -1,126 +1,83 @@
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+import {Button, SafeAreaView, View} from 'react-native';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, {
+  useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const width = useSharedValue(1);
-  const animatedStyle = useAnimatedStyle(() => {
-    console.log('Running on the UI thread');
+const App: React.FC = ({}) => {
+  const height = useSharedValue<number>(300);
+  const offset = useSharedValue<number>(100);
+  const pressed = useSharedValue<boolean>(false);
+  const x = useSharedValue<number>(0);
+  const y = useSharedValue<number>(0);
+  const animatedSBlue = useAnimatedStyle(() => {
     return {
-      opacity: width.value,
+      height: height.value,
     };
   });
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  useEffect(() => {
-    setInterval(() => {
-      width.value = width.value - 0.1;
-    }, 1000);
-    return () => {};
-  }, [width]);
+  const animatedSRed = useAnimatedStyle(() => {
+    return {
+      height: 100,
+      width: 40,
+      backgroundColor: 'red',
+      left: offset.value,
+    };
+  });
+  const eventHandler = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      pressed.value = true;
+    },
+    onActive: (event, ctx) => {
+      x.value = 100 + event.translationX;
+      y.value = 100 + event.translationY;
+    },
+    onEnd: (event, ctx) => {
+      pressed.value = false;
+      x.value = withSpring(100);
+      y.value = withSpring(100);
+    },
+  });
+  const uas = useAnimatedStyle(() => {
+    return {
+      backgroundColor: pressed.value ? '#FEEF86' : '#001972',
+      transform: [{translateX: x.value}, {translateY: y.value}],
+    };
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Animated.View
-        style={[{height: 50, backgroundColor: 'blue'}, animatedStyle]}
+    <SafeAreaView style={{}}>
+      <View style={{height: 300}}>
+        <Animated.View
+          onTouchEnd={() => {
+            height.value = withRepeat(
+              withTiming(0, {duration: 600}),
+              100,
+              true,
+            );
+          }}
+          style={[{backgroundColor: 'blue'}, animatedSBlue]}
+        />
+      </View>
+      <Animated.View style={animatedSRed} />
+      <Button
+        onPress={() => {
+          offset.value = withSpring(Math.random() * 300, {}, finished => {});
+        }}
+        title="Move"
       />
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <PanGestureHandler onGestureEvent={eventHandler}>
+        <Animated.View
+          style={[{height: 50, width: 50, borderRadius: 50}, uas]}
+        />
+      </PanGestureHandler>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
